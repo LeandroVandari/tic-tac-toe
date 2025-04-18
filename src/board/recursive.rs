088169d681442db6edf3bdca2e8ed0d4d142b5c1
LeandroVandari <bernardokreutz@gmail.com>
2025-04-17 22:53:39 -0300

    Added docs for RecursiveCell and RecursiveBoard

--- src/board/recursive.rs
+++ src/board/recursive.rs
@@ -5,12 +5,15 @@ use crate::{BoardResult, BoardState};
 use super::{Board, BoardDisplay, cell::Cell, inner::InnerBoard};
 pub use cell::RecursiveCell;
 
+/// A game board that contains game boards of itself. Each cell is stored as a [`RecursiveCell`], which
+/// then contains the [InnerBoard] for that cell.
 pub struct RecursiveBoard {
     cells: [RecursiveCell; 9],
 }
 
 impl RecursiveBoard {
     #[must_use]
+    /// Returns a fresh [`RecursiveBoard`], with all cells empty.
     pub const fn new() -> Self {
         Self {
             cells: [const { RecursiveCell::new() }; 9],
@@ -48,6 +51,13 @@ pub mod cell {
     use super::*;
 
     #[derive(Debug, Clone)]
+    /// The type that actually allows for us to have a [`RecursiveBoard`].
+    ///
+    /// Each [`RecursiveCell`] is made out of two components: The `board` and the `state`.
+    ///
+    /// The former contains the individual game itself, represented by an [`InnerBoard`],
+    /// whilst the latter is basically a cache for the [`BoardState`] returned by the `board`'s
+    /// [`Board::get_state`], so it doesn't need to be updated all the time.
     pub struct RecursiveCell {
         board: InnerBoard,
         pub(super) state: BoardState,
@@ -55,6 +65,7 @@ pub mod cell {
 
     impl RecursiveCell {
         #[must_use]
+        /// Returns a [`RecursiveCell`] with a completely empty board.
         pub const fn new() -> Self {
             Self {
                 board: InnerBoard::new(),
