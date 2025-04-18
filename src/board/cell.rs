7424d58c9607416a3141608732d6661e1cdda473
LeandroVandari <bernardokreutz@gmail.com>
2025-04-17 22:58:36 -0300

    Add documentation to the Cell trait

--- src/board/cell.rs
+++ src/board/cell.rs
@@ -1,6 +1,14 @@
 use crate::Player;
 
+/// The trait that cells for [`super::Board`] implementers must have.
+///
+/// It allows for the generic implementations of [`super::Board::get_state`] and
+/// [`super::BoardDisplay`].
 pub trait Cell {
+    /// Returns the [`Player`] who owns the cell. If the cell is empty or a drawn/in-progress
+    /// game, return [`None`].
     fn owner(&self) -> Option<&Player>;
+    /// Returns the [`Cell`]'s representation as a [`char`]. Required to be able to have more
+    /// nuanced representations of cells by [`super::BoardDisplay`].
     fn as_char(&self) -> char;
 }
